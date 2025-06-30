d98463764335cc91cb149bbba2ab6306e4fcfdb1
Barisaku <yuki@bariton1.net>
2025-07-01 06:59:34 +0900

    エンコードファイル追加

--- setup.sh
+++ setup.sh
@@ -4,8 +4,9 @@
 # cd docker-mirakurun-epgstation
 cp docker-compose-sample.yml docker-compose.yml
 cp epgstation/config/enc.js.template epgstation/config/enc.js
-cp epgstation/config/enc_software.js.template epgstation/config/enc_software.js
+cp epgstation/config/enc_hevc.js.template epgstation/config/enc_hevc.js
 cp epgstation/config/enc_vaapi.js.template epgstation/config/enc_vaapi.js
+cp epgstation/config/enc_vaapi_hevc.js.template epgstation/config/enc_vaapi_hevc.js
 cp epgstation/config/config.yml.template epgstation/config/config.yml
 cp epgstation/config/operatorLogConfig.sample.yml epgstation/config/operatorLogConfig.yml
 cp epgstation/config/epgUpdaterLogConfig.sample.yml epgstation/config/epgUpdaterLogConfig.yml
